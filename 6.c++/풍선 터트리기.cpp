f88ae1f9975c090cad8a7adfde8fb567ed483cce
goodlucky1215 <goodlucky1215@naver.com>
2022-03-13 11:36:46 +0900

    c++

--- 6.c++/풍선 터트리기.cpp
+++ 6.c++/풍선 터트리기.cpp
@@ -22,3 +22,31 @@ int solution(vector<int> a) {
     }
     return answer;
 }
+
+/////////////////////////////////////////////////다른 풀이법/////////////////////////////
+
+#include <string>
+#include <vector>
+#include <set>
+using namespace std;
+
+int solution(vector<int> a) {
+    int aSize = a.size();
+    if(aSize<3) return aSize;
+    set<int> se;
+    int frontNum = a[0];
+    int backNum = a[aSize-1];
+    se.insert(frontNum);
+    se.insert(backNum);
+    for(int i=1,j=aSize-2;i<aSize;i++,j--){
+        if(frontNum>a[i]) {
+            frontNum = a[i];
+            se.insert(frontNum);
+        }
+        if(backNum>a[j]){
+            backNum = a[j];
+            se.insert(backNum);
+        }
+    }
+    return se.size();
+}
