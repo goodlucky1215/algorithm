12b5b2bec63ed6911d2edd57d80a675e997e4dfe
goodlucky1215 <goodlucky1215@naver.com>
2022-05-09 20:36:56 +0900

    15591

--- 6.c++/15591.cpp
+++ 6.c++/15591.cpp
@@ -1,72 +1,43 @@
 #include <iostream>
 #include <queue>
+#include <map>
 using namespace std;
 
-int mootube[5001][5001];
-int n;
+vector<map<int,int>> mootube(5001);
 
 int getMooTubeSu(int k,int v){
   int result = 0;
-  for(int i =1;i<n;i++){
-    if(mootube[v][i]>=k) result++;
+  bool check[5001] = {0,};
+  check[v] = 1;
+  queue<int> quMooTube;
+  quMooTube.push(v);
+
+  while(!quMooTube.empty()){
+    int v1 = quMooTube.front();
+    quMooTube.pop();
+    
+    for(auto iter = mootube[v1].begin(); iter!=mootube[v1].end();iter++){
+      if(check[iter->first]==0 and iter->second >=k) {
+        quMooTube.push(iter->first);
+        check[iter->first]=1;
+        result++;
+      }
+    }    
   }
+  
   return result;
 }
 
-void minNetwork(int start,int end){
-  bool su[n];
-  queue<int> qu;
-  queue<int> quMin;
-  for(int i=1;i<n;i++){
-    su[i] = false;
-    if(mootube[start][i]!=0){
-      qu.push(i);
-      quMin.push(mootube[start][i]);
-    }
-  }
-  mootube[start][end] = 1000000001;
-  su[start] = true;
-  while(!qu.empty()){
-    int s = qu.front();
-    int sMin = quMin.front();
-    qu.pop();
-    quMin.pop();
-    su[s] = true;
-    for(int i=1;i<n;i++){
-      if(i==end and mootube[s][i]!=0){
-        mootube[start][end] = min(mootube[s][i],mootube[start][end]);
-        mootube[start][end] = min(sMin,mootube[start][end]);
-        continue;
-      }
-      if(su[i]==false and mootube[s][i]!=0){
-        qu.push(i);
-        quMin.push(min(mootube[s][i],sMin));
-      }
-    }
-  }
-  mootube[end][start] = mootube[start][end];
-}
-
 void mootubeStart(){
-  cin>>n;
+  int n,q;
+  cin>>n>>q;
   n++;
-  int q;
-  cin>>q;
 
   for(int i=0;i<n-2;i++){
     int t1, t2, usado;
     cin>>t1>>t2>>usado;
-    mootube[t1][t2] = usado;
-    mootube[t2][t1] = usado;    
-  }
-
-  //모든 동영상의 최솟값의 연관성을 찾아준다.
-  for(int i=1;i<n;i++){
-    for(int j=1;j<n;j++){
-      if(i==j) continue;
-      if(mootube[i][j]!=0) continue;
-      minNetwork(i,j);
-    }
+    mootube[t1][t2] = usado;  
+    mootube[t2][t1] = usado;  
   }
   
   //q개의 질문, 동영상 v와 관련된 동영상이 k이상인 동영상의 수
