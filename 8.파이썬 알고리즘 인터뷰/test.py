15663fa6e63ee8a81220b5d95d2225985cb680cf
jiny <goodlucky1215@naver.com>
2023-04-18 23:57:42 +0900

    python study

--- 8.파이썬 알고리즘 인터뷰/test.py
+++ 8.파이썬 알고리즘 인터뷰/test.py
@@ -1,2 +1,14 @@
-print("안녕하세요.")
-print("안녕히계세요.")
+#1-1 : 루프
+sum = 0
+for i in range(1,10+1):
+    sum +=i
+print("sum : ", sum);
+
+#1-2 : 루프
+#sum = sum(i for i in range(1,10+1))
+#print("sum : ", sum);
+
+#1-3 : 루프
+sum = 0
+sum = sum(range(1,10+1))
+print("sum : ", sum);
