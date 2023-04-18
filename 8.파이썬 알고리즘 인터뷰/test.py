987c41f8f9f10f7af48fa30090a29e5807641882
jiny <goodlucky1215@naver.com>
2023-04-18 23:59:06 +0900

    파이썬 : loop

--- 8.파이썬 알고리즘 인터뷰/test.py
+++ 8.파이썬 알고리즘 인터뷰/test.py
@@ -1,14 +1,13 @@
 #1-1 : 루프
-sum = 0
+sum1 = 0
 for i in range(1,10+1):
-    sum +=i
-print("sum : ", sum);
+    sum1 +=i
+print("sum : ", sum1);
 
 #1-2 : 루프
-#sum = sum(i for i in range(1,10+1))
-#print("sum : ", sum);
+sum1 = sum(i for i in range(1,10+1))
+print("sum : ", sum1);
 
 #1-3 : 루프
-sum = 0
-sum = sum(range(1,10+1))
-print("sum : ", sum);
+sum1 = sum(range(1,10+1))
+print("sum : ", sum1);
