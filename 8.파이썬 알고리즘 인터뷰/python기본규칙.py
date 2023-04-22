cf6f46482b79cf9a1caaef2b8c67a13a1c3bd313
jiny <goodlucky1215@naver.com>
2023-04-22 22:51:08 +0900

    python 기본 규칙 studing:

--- 8.파이썬 알고리즘 인터뷰/python기본규칙.py
+++ 8.파이썬 알고리즘 인터뷰/python기본규칙.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def long_function_name(
         var_one, var_two,
         var_three, var_four):
@@ -65,3 +68,61 @@ print(a)
 # 예를 들어 숫자 1억개를 계산하는 프로그램이 있다면
 # 제너레이터가 없다면 메모리 어딘가에 숫자 1억개를 보관해야한다.
 # 제너레이터가 있다면 단순히 제너레이터만 생성해두고 필요할 때 언제든 숫자를 만들어낼 수 있다.
+def get_natural_number():
+    n = 0
+    while True:
+        n += 1
+        yield n
+# 100개의 값을 생성하고 싶다면 100번동안 next()를 수행하면 된다.
+g = get_natural_number()
+for _ in range(0,100):
+    print(next(g))
+# 제너레이터는 여러 타입의 값을 하나의 함수에서 생성하는 것도 가능하다.
+def generator():
+    yield 1
+    yield 'string'
+    yield True
+g = generator()
+print(next(g))
+print(next(g))
+print(next(g))
+
+#6. range() : 제너레이터 방식을 쓰는 대표적인 함수
+# 파이썬 버전 3.x 이후로 range() 함수가 제너레이터 역할을 하는 range 클래스를 리턴하는 형태가 됨.
+a = list(range(5))
+print(a)
+for i in range(5):
+    print(i, end=' ')
+print()
+# 숫자 100만개를 생성하는 방법 2가지
+a = [n for n in range(1000000)]
+b = range(1000000)
+print(len(a) == len(b)) # True : 둘 다 100만개로 나옴
+print('a List : ',type(a),', b List : ', type(b)) # a List :  <class 'list'> , b List :  <class 'range'>
+print('a size : ',sys.getsizeof(a),', b size : ', sys.getsizeof(b)) # a size :  8448728 , b size :  48
+# => 똑같이 100만개를 생성하지만,
+# => range 사이즈(메모리 점유율)가 훨씬 작을 것을 알 수 있다.
+# why? range는 값을 보관하는게 아닌 생성 조건만을 보관하기 때문이다. 인덱스 접근도 가능하다. 그때 바로 생성해서 구현된다.
+
+#7. enumerate : 여러 가지 자료형(list, set, tuple 등)을 인덱스를 포함한 enumerate 객체로 리턴한다.
+a = [1,45,'a','c']
+enumerate(a)
+lists = list(enumerate(a))
+print(lists) # [(0, 1), (1, 45), (2, 'a'), (3, 'c')]
+#리스트를 인덱스와 함께 값 출력하는 방법
+a = ['a1','a2','a3']
+#방법1
+for i in range(len(a)):
+    print(i, a[i], "/", end=' ')
+print()
+#방법2
+i = 0
+for v in a:
+    print(i, v, "/", end=' ')
+    i+=1
+print()
+#방법3
+for i, v in enumerate(a):
+    print(i, v, "/", end=' ')
+print()
+
