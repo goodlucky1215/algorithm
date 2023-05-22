310b7cca47fe43415877c69d9be3ff49b7923318
jiny <goodlucky1215@naver.com>
2023-05-22 23:32:05 +0900

    브루트포스

--- 8.파이썬 알고리즘 인터뷰/7장.배열/1. Two Sum.py
+++ 8.파이썬 알고리즘 인터뷰/7장.배열/1. Two Sum.py
@@ -1,7 +1,19 @@
+#브루트포스
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_len = len(nums);
-        nums_index = sorted(range(nums_len), key= lambda k: nums[k])
-        frist, second = 0,1
-        while():
-            target_find = nums[nums_index[frist]]+nums[nums_index[second]];
+    def twoSum1(self, nums: List[int], target: int) -> List[int]:
+        nums_len = len(nums)
+        for first in nums_len-1:
+            for second in range(first+1,nums_len):
+                if(nums[first]+nums[second]==target) :
+                    return [first, second]
+        return None;
+
+#브루트포스와 같은 시간복잡도로 같지만, if문에서 ==보다는 in이 빠르다.
+class Solution:
+    def twoSum2(self, nums: List[int], target: int) -> List[int]:
+        nums_len = len(nums)
+        for index, num in enumerate(nums):
+            su = target - num
+            if su in nums[index+1:]:
+                return [first, second]
+        return None;
