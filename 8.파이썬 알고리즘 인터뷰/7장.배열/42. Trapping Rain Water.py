44d0eea3df7f1db587ef20d293428553d6434034
jiny <goodlucky1215@naver.com>
2023-05-29 23:25:28 +0900

    stack -ing

--- 8.파이썬 알고리즘 인터뷰/7장.배열/42. Trapping Rain Water.py
+++ 8.파이썬 알고리즘 인터뷰/7장.배열/42. Trapping Rain Water.py
@@ -3,18 +3,3 @@ class Solution:
         volum = 0
         stack  = []
         for i in range(len(height)) :
-            if height[i]!=0:
-                stack.append[height[i]]
-                start = i+1
-                break
-        if(len(height)==start) : return 0
-        for i in height[start:]:
-            if stack[-1]>i:
-                stack.append(i)
-            while stack[-1]<i:
-                if len(stack)==1:
-                    stack.pop();
-                    stack.append(i)
-
-
-
