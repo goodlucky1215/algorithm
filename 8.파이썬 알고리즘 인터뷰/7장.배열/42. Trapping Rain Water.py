71e824139a3a81d023bdc422d882ea8438fca358
jiny <goodlucky1215@naver.com>
2023-05-31 23:54:02 +0900

    stack study

--- 8.파이썬 알고리즘 인터뷰/7장.배열/42. Trapping Rain Water.py
+++ 8.파이썬 알고리즘 인터뷰/7장.배열/42. Trapping Rain Water.py
@@ -1,5 +1,16 @@
+# stack을 이용
 class Solution:
     def trap(self, height: List[int]) -> int:
-        volum = 0
+        result = 0
         stack  = []
         for i in range(len(height)) :
+            while stack and height[i]>=height[stack[-1]] :
+                j = stack.pop()
+                if len(stack) == 0 : break
+                distance = i - stack[-1] - 1
+                water = min(height[i],height[stack[-1]])-height[j]
+                result += distance*water
+            stack.append(i)
+        return result
+    
+#투포인터
