7dfda4f681a2140ff7c3fe2ba816c30030b96a59
jiny <goodlucky1215@naver.com>
2023-06-02 23:14:28 +0900

    투포인터 study

--- 8.파이썬 알고리즘 인터뷰/7장.배열/42. Trapping Rain Water.py
+++ 8.파이썬 알고리즘 인터뷰/7장.배열/42. Trapping Rain Water.py
@@ -16,14 +16,17 @@ class Solution:
 #투포인터
 class Solution:
     def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
         result = 0
-        stack  = []
-        for i in range(len(height)) :
-            while stack and height[i]>=height[stack[-1]] :
-                j = stack.pop()
-                if len(stack) == 0 : break
-                distance = i - stack[-1] - 1
-                water = min(height[i],height[stack[-1]])-height[j]
-                result += distance*water
-            stack.append(i)
+        left, right = 0,len(height)-1
+        left_max, right_max = height[0], height[-1]
+        while left < right:
+            left_max, right_max = max(left_max,height[left]), max(right_max,height[right])
+            if left_max<=right_max:
+                result += left_max-height[left]
+                left+=1
+            else:
+                result += right_max-height[right]
+                right-=1
         return result
