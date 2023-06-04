346bd2f8de1e502aa39ad58609440b6243cc517a
jiny <goodlucky1215@naver.com>
2023-06-04 23:52:50 +0900

    브루트포스

--- 8.파이썬 알고리즘 인터뷰/7장.배열/15. 3Sum.py
+++ 8.파이썬 알고리즘 인터뷰/7장.배열/15. 3Sum.py
@@ -1,17 +1,20 @@
-#브루트포스 :
+#브루트포스 : 타임아웃
 import collections
 from typing import List
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        result = set()
+        result = []
+        nums.sort()
         for i in range(0, len(nums)-2):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
             for j in range(i+1, len(nums) - 1):
-                nums_add = nums[i]+nums[j]
-                for k in range(j+1, len(nums)) :
-                    if(nums_add + nums[k])==0:
-                        lists = [nums[i],nums[j],nums[k]]
-                        lists.sort()
-                        result.add(tuple(lists))
-        result_list = [list(t) for t in result]
-        return result_list
+                if j>i+1 and nums[j]==nums[j-1]:
+                    continue
+                for k in range(j+1, len(nums)):
+                    if k>j+1 and nums[k]==nums[k-1]:
+                        continue
+                    if(nums[i] + nums[j] + nums[k])==0:
+                        result.append([nums[i],nums[j],nums[k]])
+        return result
 
