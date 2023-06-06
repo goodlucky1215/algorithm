93ca4819099bc2ece40218b7780a673826e42713
jiny <goodlucky1215@naver.com>
2023-06-07 00:29:50 +0900

    투포인터

--- 8.파이썬 알고리즘 인터뷰/7장.배열/15. 3Sum.py
+++ 8.파이썬 알고리즘 인터뷰/7장.배열/15. 3Sum.py
@@ -18,3 +18,31 @@ class Solution:
                         result.append([nums[i],nums[j],nums[k]])
         return result
 
+#투 포인터
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums.sort()
+
+        for i in range(0, len(nums)-2):
+            left, right = i+1,len(nums)-1
+            if nums[i]>0: break
+            if (i>0 and nums[i]==nums[i-1]) : continue
+            while left<right:
+                if nums[i]+nums[left]+nums[right]>0:
+                    right-=1
+                    while left<right and nums[right]==nums[right+1]:
+                        right-=1
+                elif nums[i]+nums[left]+nums[right]<0:
+                    left+=1
+                    while left<right and nums[left]==nums[left-1]:
+                        left += 1
+                else:
+                    result.append([nums[i], nums[left], nums[right]])
+                    while left<right and nums[right]==nums[right+1]:
+                        right-=1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    right -= 1
+                    left += 1
+        return result
