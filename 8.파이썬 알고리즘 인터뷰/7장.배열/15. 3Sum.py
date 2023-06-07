7c107085726e470f517915a4673df6569a81e50d
jiny <goodlucky1215@naver.com>
2023-06-07 22:42:05 +0900

    two pointer

--- 8.파이썬 알고리즘 인터뷰/7장.배열/15. 3Sum.py
+++ 8.파이썬 알고리즘 인터뷰/7장.배열/15. 3Sum.py
@@ -31,17 +31,13 @@ class Solution:
             while left<right:
                 if nums[i]+nums[left]+nums[right]>0:
                     right-=1
-                    while left<right and nums[right]==nums[right+1]:
-                        right-=1
                 elif nums[i]+nums[left]+nums[right]<0:
                     left+=1
-                    while left<right and nums[left]==nums[left-1]:
-                        left += 1
                 else:
                     result.append([nums[i], nums[left], nums[right]])
-                    while left<right and nums[right]==nums[right+1]:
+                    while left<right and nums[right]==nums[right-1]:
                         right-=1
-                    while left < right and nums[left] == nums[left - 1]:
+                    while left < right and nums[left] == nums[left+1]:
                         left += 1
                     right -= 1
                     left += 1
