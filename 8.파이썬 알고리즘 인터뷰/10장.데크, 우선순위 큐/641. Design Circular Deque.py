0b650a04f69c5d591f36aae71788f4675c816d24
jiny <goodlucky1215@naver.com>
2023-07-24 23:22:57 +0900

    deque

---
+++ 8.파이썬 알고리즘 인터뷰/10장.데크, 우선순위 큐/641. Design Circular Deque.py
@@ -0,0 +1,21 @@
+
+
+        class MyCircularDeque:
+
+            def __init__(self, k: int):
+
+            def insertFront(self, value: int) -> bool:
+
+            def insertLast(self, value: int) -> bool:
+
+            def deleteFront(self) -> bool:
+
+            def deleteLast(self) -> bool:
+
+            def getFront(self) -> int:
+
+            def getRear(self) -> int:
+
+            def isEmpty(self) -> bool:
+
+            def isFull(self) -> bool:
