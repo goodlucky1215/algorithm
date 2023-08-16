c09439a5995f11ff29d86349f00c981227f67047
jiny <goodlucky1215@naver.com>
2023-08-16 17:39:19 +0900

    다익스트라

--- 8.파이썬 알고리즘 인터뷰/13장,최단 경로 문제/743. Network Delay Time.py
+++ 8.파이썬 알고리즘 인터뷰/13장,최단 경로 문제/743. Network Delay Time.py
@@ -1,4 +1,5 @@
 import collections
+import heapq
 from typing import List
 
 
@@ -6,34 +7,43 @@ class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         #처음에 값을 다 담는다.
         load = collections.defaultdict(list)
+        #결과
+        result = -1
+
         for i in times:
             load[i[0]].append([i[2],i[1]])
-            load[i[1]].append([i[2],i[0]])
-
-        #작은 값부터 정렬한다.
-        for i in list(load):
-            load[i].sort()
-
-        #체크
-        result = -1
 
         # k -> i 로 가는 것을 하나씩 찾아나가면서, 죄댓값을 갱신한다.
         # 그러다가 값이 없으면 -1을 return 시켜준다.
         for i in range(1,n+1):
             if i==k:
                 continue
-            deq = collections.deque()
-            deq.append(load[k][0])
-            load_all = 0
-            while deq :
-                x,y = deq.popleft()
+
+            he = []
+            for j in load[k]:
+                heapq.heappush(he,j)
+
+            load_check = []
+            for j in range(0,n+1):
+                load_check.append([False]*(n+1))
+
+            check = False
+
+            while he :
+                x,y = heapq.heappop(he)
                 if y==i:
-                    result = max(result, load_all+x)
+                    result = max(result, x)
+                    check = True
                     break
+                for j in load[y]:
+                    if not load_check[y][j[1]] :
+                        load_check[y][j[1]] = True
+                        load_check[j[1]][y] = True
+                        heapq.heappush(he,[j[0]+x,j[1]])
+            if not check:
+                return -1
 
+        return result
 
-
-        return 1
-
-Solution().networkDelayTime([[2,1,1],[2,3,1],[3,4,1]],4,2)
+print(Solution().networkDelayTime([[2,1,1],[2,3,1],[3,4,1]],4,2))
 
